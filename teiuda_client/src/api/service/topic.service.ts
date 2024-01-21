228bd0bf8a25d296002d15335a06a9b041793de9
icefirebear <50494659+WoungSub-Byun@users.noreply.github.com>
2024-01-21 14:06:41 +0000

    add: add topic service

--- teiuda_client/src/api/service/topic.service.ts
+++ teiuda_client/src/api/service/topic.service.ts
@@ -1,5 +1,7 @@
+import ITopicData from "@/types/topic.type";
 import authHeader from "./auth-header";
 import axiosClient from "@/api";
+
 class TopicService {
   getTodayTopic() {
     return axiosClient.get("/today", {
