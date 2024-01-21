196724e1adc1f063b456f06ff51927a7205b297b
icefirebear <50494659+WoungSub-Byun@users.noreply.github.com>
2024-01-21 14:05:18 +0000

    add: add thinkboard component

--- teiuda_client/src/components/Think/ThinkBoard/index.tsx
+++ teiuda_client/src/components/Think/ThinkBoard/index.tsx
@@ -1,19 +1,51 @@
 import styled from "styled-components";
+import * as mock_data from "./mock.json";
+import ThinkService from "@/api/service/think.service";
+import TopicService from "@/api/service/topic.service";
+import { useEffect, useState } from "react";
+import { Topic } from "@/components/Think/Topic";
 
-const ThinkBoard = (props: { isIntro: boolean }) => {
+const MockData = mock_data;
+
+interface ThinkCellDataType {
+  user: string;
+  content: string;
+}
+
+const ThinkBoard = (props: { isLoggedIn: boolean }) => {
+  const [thinkData, setThinkData] = useState<ThinkCellDataType[]>([]);
+
+  useEffect(() => {
+    getThinkData();
+  }, []);
+
+  const getThinkData = () => {
+    if (props.isLoggedIn) {
+      ThinkService.getToday()
+        .then((res: any) => {
+          let tmp: ThinkCellDataType[] = [];
+          res.forEach((data: any) => {
+            tmp.push({ user: data.uid, content: data.content });
+          });
+          setThinkData(tmp);
+        })
+        .catch((e: Error) => {
+          console.log(e);
+        });
+    } else {
+      let tmp: ThinkCellDataType[] = [];
+      MockData.think.forEach((data: any) => {
+        tmp.push({ user: data.user, content: data.content });
+      });
+      console.log("되나?");
+      setThinkData(tmp);
+    }
+  };
+
   const Wrapper = styled.div`
     display: inline;
   `;
-  const Topic = styled.span`
-    margin: 0 auto;
-    color: #0d3c26;
 
-    font-family: "NanumMyeongjo";
-    text-align: center;
-    font-size: 300%;
-    font-style: normal;
-    font-weight: 400;
-  `;
   const Divider = styled.span`
     position: relative;
     top: 20%;
@@ -22,11 +54,21 @@ const ThinkBoard = (props: { isIntro: boolean }) => {
     border: 0;
     background-color: #735a19;
   `;
+
   return (
     <Wrapper>
-      <Topic> 토픽 </Topic>
+      <Topic isLoggedIn={props.isLoggedIn} />
       <Divider />
-      {props.isIntro ? <div>Demo Content </div> : <div> Real Content </div>}
+      <div className="think-board">
+        {thinkData.map((data: ThinkCellDataType) => {
+          return (
+            <div className="think-cell">
+              <div>{data.content}</div>
+              <div>{data.user}</div>
+            </div>
+          );
+        })}
+      </div>
     </Wrapper>
   );
 };
